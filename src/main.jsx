3d8eadb40550b5b55427bc0abb51cbdfb1b14d8f
laag-dev <laag.08@gmail.com>
2023-10-07 06:01:16 -0300

    Proyecto terminado

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
 import { ProviderListCartContext } from "./context/ListCartContext";
 import { AuthProvider } from "../src/context/AuthContext";
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router basename="/tienda-coder">
   <AuthProvider>
   <ProviderListCartContext>
     <BrowserRouter>
@@ -15,4 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </ProviderListCartContext>
   </AuthProvider>
+  </Router>
 );
