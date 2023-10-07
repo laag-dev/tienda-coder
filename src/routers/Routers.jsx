2bbca53b5d65b50f0be99b728faf2128421c885b
laag-dev <laag.08@gmail.com>
2023-10-07 15:51:46 -0300

    Proyecto terminado

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -25,8 +25,8 @@ export const Routers = () => {
   return (
     
     <Routes>
-      <Route path="/" element={<Navigate to="home" />} />
-     
+      
+      <Route path="/" element={<Home />} />
       <Route path="electronics" element={<Electronics/>} />
       <Route path="jewelery" element={<Jewelery/>} />
       <Route path="furniture" element={<Furniture/>} />
