566a0d12114eaad17f1768c8ca34ba19438777d8
laag-dev <laag.08@gmail.com>
2023-10-07 04:24:18 -0300

    Proyecto terminado

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tienda-coder/',
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
