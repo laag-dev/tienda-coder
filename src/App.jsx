999d4f1e0a90f53c94d45b741d81cbb01afba4df
laag-dev <laag.08@gmail.com>
2023-10-07 15:57:10 -0300

    Proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Layout } from './components/layout/Layout';
+import { Home } from "./pages/Home";
 
 function App() {
   return (
      <>
-     <Layout/>
+     <Home/>
      </>
      
   );
